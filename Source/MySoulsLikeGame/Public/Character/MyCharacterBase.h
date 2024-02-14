870b4ee5d4da6d9d0c88bf7f17a78f20d1739821
sunyuxin <2503880658@qq.com>
2024-02-14 15:35:09 +0800

    the Base Character Class

--- Source/MySoulsLikeGame/Public/Character/MyCharacterBase.h
+++ Source/MySoulsLikeGame/Public/Character/MyCharacterBase.h
@@ -6,24 +6,15 @@
 #include "GameFramework/Character.h"
 #include "MyCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class MYSOULSLIKEGAME_API AMyCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
+public:	
 	AMyCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
